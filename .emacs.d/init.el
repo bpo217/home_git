408faf8f2e73d8a373163e889421af3be778c8c8
Benjamin Olson <venus@pop-os.localdomain>
2019-01-24 23:30:07 -0800

    Updated Emacs 25 and packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,10 @@
  show-paren-delay 0.5 ;;I don't know what this does
  use-package-always-ensure t
  make-backup-files nil
+ split-height-threshold nil ;;the next two attempt to split vertically always instead of horizontally
+ split-width-threshold 80
  tool-bar-mode 0) ;;No clickable toolbar, use hotkeys
+(tool-bar-mode -1)
 (global-linum-mode 1) ;;This may be the same as line-number-mode
 (toggle-frame-maximized)
 (desktop-save-mode 1)
@@ -18,10 +21,7 @@
 ;; Easy to remove them all with dired
 
 ;;Default font
-(set-default-font "Anonymous Pro 14")
-
-;;Set the theme
-(load-theme 'tango-dark)
+(set-default-font "SF Mono 14")
 
 ;; buffer local variables
 (setq-default
@@ -52,10 +52,15 @@
   (package-install 'use-package))
 (require 'use-package)
 
+;;Set the theme
+(use-package "dracula-theme")
+;;(load-theme 'dracula)
+
 ;;Scala mode
-(use-package scala-mode
-  :interpreter
-  ("scala" . scala-mode))
+;;(use-package scala-mode
+;;  :interpreter
+;;  ("scala" . scala-mode))
+(use-package nim-mode)
 (use-package typescript-mode)
 (use-package feature-mode)
 (custom-set-variables
@@ -63,9 +68,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default)))
  '(package-selected-packages
    (quote
-    (sass-mode use-package typescript-mode scala-mode feature-mode)))
+    (cargo nim-mode dracula-theme sass-mode use-package typescript-mode feature-mode)))
  '(typescript-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
